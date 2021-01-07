e3683d3090543bd9949439ed6c7ce607e0bfb297
= <=>
2021-01-07 09:21:53 +0700

    finished

---
+++ server/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import postRoute from "./routes/posts.js";
+
+const app = express();
+
+dotenv.config();
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRoute);
+
+app.get("/", (req, res) => {
+  res.send("Hello to Memories!");
+});
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`))
+  )
+  .catch((e) => console.log(e.message));
+
+mongoose.set("useFindAndModify", false);
